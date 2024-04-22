e107cbebef6d11bb302ff202f4cb3bb3efc8d9ee
RuhamaMalik <malikruhama7@gmail.com>
2024-04-22 13:09:56 +0500

    alert, console , comments, document.write, variables

---
+++ javaScript/variables/index.js
@@ -0,0 +1,46 @@
+// alert('Hello Ruhama');
+// Alert('Hello Ruhama'); // error
+
+// var myMessage = "hello baji";
+// alert(myMessage);
+
+// single line comment
+
+/*
+Multi
+line 
+comment
+*/
+
+// window.alert("Hello Ruhama Gull")
+
+
+/////////////////////// Variables
+
+// var username = "Ruhama Gull";
+
+// console.log("Direct message");
+// console.log("Hello" , username);
+// console.log(username)
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(username);
+
+// document.write("abcd")
+
+// = ---> assignment operator
+
+// var email; // declaration
+// email = "abcd@gmail.com"; //initialization / assignment
+// console.log(email);
+
+// var description = "my description";
+// description = "my updated description"; //reassign
+// description = "my updated description 2"
+// console.log(description);
+
+// meaning ful variable name 
+
+// var x = "Lavishka";
+// var username = "Lavishka";
