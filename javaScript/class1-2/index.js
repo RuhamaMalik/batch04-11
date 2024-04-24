046a00e7847533250d2986ec9ccad896fbf33662
RuhamaMalik <malikruhama7@gmail.com>
2024-04-24 13:07:49 +0500

    alert, console , comments, document.write, variables , operators, statements, expressions, post/preFix

---
+++ javaScript/class1-2/index.js
@@ -0,0 +1,171 @@
+// alert('Hello Ruhama');
+// Alert('Hello Ruhama'); // error
+
+// var myMessage = "hello baji";
+// alert(myMessage);
+
+// single line comment
+
+/*
+Multi
+line 
+comment
+*/
+
+// window.alert("Hello Ruhama Gull")
+
+
+/////////////////////// Variables
+
+// var username = "Ruhama Gull";
+
+// console.log("Direct message");
+// console.log("Hello" , username);
+// console.log(username)
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(username);
+
+// document.write("abcd")
+
+// = ---> assignment operator
+
+// var email; // declaration
+// email = "abcd@gmail.com"; //initialization / assignment
+// console.log(email);
+
+// var description = "my description";
+// description = "my updated description"; //reassign
+// description = "my updated description 2"
+// console.log(description);
+
+// meaning ful variable name 
+
+// var x = "Lavishka";
+// var username = "Lavishka";
+
+// premitive data types
+
+// string ""/ ''
+// number 0-9
+// boolean true/false
+// undefine 
+// null 0, ''
+
+// var  username = null;
+
+// console.log(username);
+
+// var age = 10;
+// age = 11.0 ;
+// console.log(age);
+
+
+// var isLoggedIn = true;
+// console.log(isLoggedIn);
+
+
+// var totalPrice = 0;
+// var item1 = 100;
+// var item2 = 200;
+
+// totalPrice = totalPrice + item1 + item2  
+// console.log(totalPrice);//300
+
+// var item3 = 300;
+// totalPrice = totalPrice + item3;
+
+// console.log(totalPrice);
+
+
+// .. legal
+
+// var variableName;
+// var $varN ;
+// var _varN;
+// var student1;
+// var my1student
+// var myvar ;
+
+// .. illegal
+
+// var my name;
+// var 1student
+// var var ;
+
+// var flower = 'Rose';
+// console.log(Flower); // error >>>>> case sensitive
+
+///////////// js naming convention
+
+//  var myFirstVar ;            //camelCase
+//  var my_first_var;          //snack_case
+
+// var rollNumber = 101;
+//  var section = 'A';
+
+/*
+Expression: combination of 
+
+*/
+
+// Familiar operators
+
+// var exp1 = 2 + 2; // 4
+// var exp2 = 13 -3; // 10
+// var exp3 = 5 * 5; // 25
+// var exp4 = 25 / 5; // 5
+// var exp5 = 10 % 3; // 1
+
+// console.log(exp1);
+// console.log(exp2);
+// console.log(exp3);
+// console.log(exp4);
+// console.log(exp5);
+
+/////////////// Unfamiliar operators
+
+// ++ --
+
+
+// var num1 = 1;
+// num1 = num1 + 1;
+// num1 = 1 + num;
+// console.log(num1); // 2
+
+// var num2 = 1;
+// num2 = num2 - 1;
+// console.log(num2); // 0
+
+// var num = 1;
+// num++; //  post increament / postFix
+// ++num; // pre increament / preFix
+
+// .. pre-increament
+// var num = 1;
+
+// console.log(num); // 1
+
+// num = ++num;
+
+// console.log(num); // 2
+
+
+// // .. post-increament
+
+// var num = 1;
+// console.log(num); // 1
+
+// num = num++;
+// console.log(num);
+
+var num = 1; // 4
+var result = num++ + num++ + ++num;
+   //      =  1    +  2    +   4
+
+   console.log(result);
+   console.log(num);
+
+
+
