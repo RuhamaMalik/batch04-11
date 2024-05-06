632d2bd26e77517b17213bb8efc6825de9d47a12
= <=>
2024-05-06 13:11:28 +0500

    prompt if else , comaprision operrators

---
+++ javaScript/class-04/index.js
@@ -0,0 +1,100 @@
+// var username = prompt("what is your name?", "Ruhama Gull");
+// console.log(username);
+
+// var question = "what is your age?";
+// var defaultAns = 10;
+// var userAge = +prompt(question, defaultAns)
+// console.log(userAge + 2);
+
+/////////////////////////////////// if
+
+// if(condition){
+//     /// code
+// }
+
+// if(true){
+//    console.log("if statement runing"); 
+// }
+
+//////////////// truthy values
+// if(-1){
+//     console.log("if statement runing -1"); 
+//  }
+// if(1){
+//     console.log("if statement runing 1"); 
+//  }
+//  if(" "){
+//     console.log("if statement runing space"); 
+//  }
+
+//  if("Ruhama"){
+//     console.log("if statement runing Ruhama"); 
+//  }
+
+
+//////////////// falsy values
+
+// if(false){
+//     console.log("if statement runing"); 
+//  }
+
+//  if(0){
+//     console.log("if statement runing"); 
+//  }
+//  if(""){
+//     console.log("if statement runing"); 
+//  }
+//  if(null){
+//     console.log("if statement runing"); 
+//  }
+//  if(undefined){
+//     console.log("if statement runing"); 
+//  }
+
+// var isLoggedIn = true;
+// if (isLoggedIn) {
+//     alert("loGGED IN sUCCESSFULLY!");
+// } else {
+//     alert("Invalid Email or Password!")
+// }
+
+
+// var isRain = true;
+// if(isRain){
+//     console.log("Umbrella le lo bhai")
+// }else{
+//     console.log("Sun glasses le lo bhai")
+ 
+// }
+
+
+//////// Comparison operators
+// ==   check values
+// console.log(2 == 2);  // true
+// console.log(2 == 3);  // false
+// console.log("abc" == "abc");  // true
+// console.log(2 == "2");  // true
+// console.log(true == "true");  // false
+
+// !=
+
+// console.log(2 != 3); // true
+// console.log(2 != 2); // false
+
+// ===  check data type + value
+
+// console.log(2 === "2");  // false 
+// console.log(true === "true"); // false
+
+// !==
+
+// console.log("abc" !== "ABC"); //  true
+// console.log(3 !== "3"); // true
+
+
+// var total = 10;
+// // total = total + 5
+// total -= 5;
+// console.log(total);
+
+// var a = null;
